0dde2a2f7b280aba85bedf55d1c8054efe6599ad
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 17:05:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def badge_maker(name)
+   return "Hello, my name is #{name}."
+ end
+ 
+def batch_badge_creator(names)
+   printed_names = []
+   names.each do |name|
+     printed_names << badge_maker(name)
+   end
+   return printed_names
+ end
+ 
+ def assign_rooms(speakers)
+   
